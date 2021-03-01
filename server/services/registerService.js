93e1caf741b69e0d359db99315d33de0e0602260
IsidoreTam <59669931+IsidoreTam@users.noreply.github.com>
2021-02-28 22:25:45 -0700

    Filled Register Service

--- server/services/registerService.js
+++ server/services/registerService.js
@@ -5,3 +5,46 @@
   true will redirect user to the to login.ejs
 */
 const fileService = require('./fileService')
+
+exports.authenticate = (credential)=>{
+ 
+    const {username, email} = {...credential}
+    const users = fileService.getFileContents('../data/users.json');
+    // flush the authentication
+    
+    const authUser =  users.reduce((authObj, user)=>{
+      
+    if(user.email != email){
+        authObj.validEmail = true;
+    }else{
+
+    }
+ 
+    if(user.username != username){
+        authObj.validUsername = true;
+    }else{
+
+    }
+ 
+    if(authObj.validEmail===true && authObj.validUsername===true){
+        authObj.user = user;
+    }
+          
+    return authObj
+ 
+    }, {validEmail:false, validUsername:false, user:null})
+ 
+    const auth0 = authUser.user ? {user:authUser.user}: formatErrors(authUser);
+    return auth0
+ 
+}
+const formatErrors = function(user){
+    let emailWarning = ""
+    let usernameWarning = ""
+  
+    if(user.validEmail === false){ emailWarning= `email is taken`}
+    if(user.validUsername === false){usernameWarning= `user name is taken`}
+  
+    return {user:null, emailWarning, usernameWarning}
+}
+  
