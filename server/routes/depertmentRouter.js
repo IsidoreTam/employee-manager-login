7f430aa75920053f24e9fa4ff56c77272b88f2df
IsidoreTam <59669931+IsidoreTam@users.noreply.github.com>
2021-03-13 12:18:12 -0700

    Starting files for assignment 2b

---
+++ server/routes/depertmentRouter.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const Departments = require('../controllers/Department');
+const Department = require('../controllers/Department')
+
+// Router   handle req/res http verbs
+const router = express.Router();
+const deparment = new Department('../data/employees.json')
+
+
+//commonjs modules 
+module.exports = ()=>{
+    // list of departments
+    router.get('/', (req, res)=>{
+        res.send(deparment.getDepartments())
+    })
+
+    // Single Department 
+    // GET Request (body)  
+    // query- query string  url?name=value&name=value
+    // params - params   url/name/value/name/value
+    router.get('/name/:name', (req, res)=>{  
+        res.send(deparment.getDepartment(req.params.name))
+    })
+
+    return router
+}
